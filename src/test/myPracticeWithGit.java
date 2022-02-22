e21236235de4db145eb55d69425fa45a51e204b2
AlyCoding <aly.khamidullina@gmail.com>
2022-02-21 18:31:23 -0800

    test2.21.22

---
+++ src/test/myPracticeWithGit.java
@@ -0,0 +1,10 @@
+package test;
+
+public class myPracticeWithGit {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 100;
+        System.out.println(num1+num2);
+    }
+}
